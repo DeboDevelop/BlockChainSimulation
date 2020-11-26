93a25cc54aa6bb6a759383791423c1ea8f3f9e4f
DeboDevelop <debajyotidofficial@gmail.com>
2020-11-26 15:58:43 +0530

    Completed the Miner Class.

--- blockchain.py
+++ blockchain.py
@@ -65,6 +65,7 @@ class Network:
         self.nodes.append(node)
 
     def verify_blockchain(self):
+        #Need to recheck 
         current = 1
         while(current < len(self.blockchain)):
             prev = self.blockchain[current-1]
@@ -86,6 +87,7 @@ class Network:
             print(" ")
 
 """
+#Block in mempool
 block_schema = {
     "transaction_id" : some_id,
     "prev_transaction_ids" : [ some_id, some_id, some_id ],
@@ -104,18 +106,25 @@ class Miner:
     nodes = []
     mempool = []
     wallet = 0
+    computation_power = 0
 
-    def __init__(self, network):
+    def __init__(self, network, coins, comp_power):
         self.miner_id = str(uuid.uuid4())
         self.blockchain = network.blockchain
         self.nodes = network.nodes
         self.mempool = []
-        self.wallet = 0
+        self.wallet = coins
+        self.computation_power = comp_power
 
     def add_node(self, node):
         self.nodes.append(node)
 
     def create_transaction(self, coins, receiver, transaction_fee):
+        #Creating a new Transactions
+        if(wallet < coins):
+            print("Not enough money in the wallet")
+            return
+        wallet-=coins
         prev_transaction_ids = self.blockchain[-1][0]["prev_transaction_ids"]
         prev_transaction_ids.append(self.blockchain[-1][0]["transaction_id"])
         random.seed(coins)
@@ -126,17 +135,19 @@ class Miner:
             "receiver_id" : receiver,
             "coins" : coins,
             "transaction_fee" : transaction_fee,
-            "incentive" : random.random()*100, 
-            "work_needed" : random.random()*100 
+            "incentive" : int(random.random()*100), 
+            "work_needed" : int(random.random()*100) 
         }
         self.mempool.append(block)
         self.request_block(block)
 
     def request_block(self, block):
+        #Request all other nodes to create this block
         for i in range(self.nodes):
             self.nodes[i].receive_transaction(block)
 
     def receive_transaction(self, block):
+        #All other nodes receiving this block and added it in their mempool
         self.mempool.append(block)
 
     def add_block_to_network(self, block, network):
@@ -163,18 +174,41 @@ class Miner:
             self.nodes[i].receive_block(block)
 
     def receive_block(self, block):
-        pass
-        #Complicated Stuff
+        # Finding the Right place to push the block
+        for j in range(len(self.blockchain)):
+            for i in range(len(self.blockchain[j])):
+                if block_list[i]["transaction_id"] == block["prev_transaction_ids"][-1]:
+                    #If it is a new block
+                    if(i == (len(block_list)-1)):
+                        self.blockchain.append([block])
+                    #If it is a new branch coming from old block
+                    else:
+                        self.blockchain[j+1].append(block)
+        self.receive_coins(block)
 
     def request_blockchain(self):
-        pass
+        #Requesting all other peers for their blockchain
+        max = 0
+        blockchain_list = 0
+        for node in self.nodes:
+            if max < len(node.blockchain):
+                max = len(node.blockchain)
+                blockchain_list = node.blockchain
+        self.blockchain = blockchain_list
 
-    def receive_coins(self):
-        for block_list in self.blockchain:
-            if block_list[0]["receiver_id"] == self.miner_id:
-                if block_list[0]["payment_received"] == False:
-                    wallet += block_list[0]["coins"]
-                    block_list[0]["payment_received"] == True
+    def receive_coins(self, block):
+        #Unloading coins from the newly created block.
+        if block["receiver_id"] == self.miner_id:
+            if block["payment_received"] == False:
+                wallet += block["coins"]
+                block["payment_received"] == True
 
-    def work(self):
-        pass
+    def work(self, network):
+        random.seed(10)
+        select = (random.random()*100)%(len(self.mempool))
+        if(mempool[select][work_needed] < self.computation_power):
+            mempool[select][work_needed] = 0
+            block = mempool.pop(select)
+            self.add_block_to_network(block, network)
+        else:
+            mempool[select][work_needed] -= self.computation_power
